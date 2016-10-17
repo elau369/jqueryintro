dc2f484f8653add2040749542f3f47b6a977e0b5
Eric Lau and Getro Naissance <getro@>
2016-10-17 15:15:23 -0700

    2nd commit:

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$("p").click(function() {
+  alert("This is a paragraph");
+});
+
+$("h1").click(function() {
+  alert("This is a header");
+  alert("I told you, THIS IS A HEADER!");
+});
+
+$("img").click(function() {
+  alert("This is an image");
+});
+});
